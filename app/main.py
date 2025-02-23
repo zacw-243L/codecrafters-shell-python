0337ae325d9682c5bffae52fbd30e5865b18e52c
zacw-243L <qwerty.amousy123442x@gmail.com>
2025-02-23 19:54:22 +0800

    TASK 8 RUN PROGRAM

--- app/main.py
+++ app/main.py
@@ -1,31 +1,54 @@
+import os
+import subprocess
 import sys
-import shutil
-
-BUILTIN_CMD = {"exit", "echo", "type"}
+from typing import Optional
 
 
-def type_cmd(command):
-    if command in BUILTIN_CMD:
-        print(f"{command} is a shell builtin")
-    elif path := shutil.which(command):
-        print(f"{command} is {path}")
+def locate_executable(command) -> Optional[str]:
+    path = os.environ.get("PATH", "")
+    for directory in path.split(":"):
+        file_path = os.path.join(directory, command)
+        if os.path.isfile(file_path) and os.access(file_path, os.X_OK):
+            return file_path
+
+
+def handle_exit(args):
+    sys.exit(int(args[0]) if args else 0)
+
+
+def handle_echo(args):
+    print(" ".join(args))
+
+
+def handle_type(args):
+    if args[0] in builtins:
+        print(f"{args[0]} is a shell builtin")
+    elif executable := locate_executable(args[0]):
+        print(f"{args[0]} is {executable}")
     else:
-        print(f"{command}: not found")
+        print(f"{args[0]} not found")
+
+
+builtins = {
+    "exit": handle_exit,
+    "echo": handle_echo,
+    "type": handle_type,
+}
 
 
 def main():
     while True:
         sys.stdout.write("$ ")
-        command = input()
-        match command.split():
-            case ["exit", "0"]:
-                exit()
-            case ["echo", *args]:
-                print(*args)
-            case ["type", cmd]:
-                type_cmd(cmd)
-            case _:
-                print(f"{command}: command not found")
+        sys.stdout.flush()
+        command, *args = input().split(" ")
+        if command in builtins:
+            builtins[command](args)
+            continue
+        elif executable := locate_executable(command):
+            subprocess.run([executable, *args])
+        else:
+            print(f"{command}: command not found")
+        sys.stdout.flush()
 
 
 if __name__ == "__main__":
