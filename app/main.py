3cb0fd2d8b5dcb2010226f43307393fde93f3ead
zacw-243L <qwerty.amousy123442x@gmail.com>
2025-02-23 19:43:59 +0800

    TASK 2

--- app/main.py
+++ app/main.py
@@ -1,13 +1,8 @@
 import sys
-
-
 def main():
-    # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
-
     # Wait for user input
-    input()
-
-
+    command = input()
+    print(f"{command}: command not found")
 if __name__ == "__main__":
     main()
