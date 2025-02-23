44d0b19f51eb900e5603c13beb1a466efa0248b6
zacw-243L <qwerty.amousy123442x@gmail.com>
2025-02-23 19:50:21 +0800

    TASK 6 TYPE

--- app/main.py
+++ app/main.py
@@ -2,19 +2,20 @@ import sys
 
 
 def main():
-    while True:
-        # Uncomment this block to pass the first stage
+    while 1:
         sys.stdout.write("$ ")
-        # Wait for user input
-        command, *args = input().split(" ")
+        command = input()
         match command:
-            case "exit":
-                break
-            case "echo":
-                print(" ".join(args))
-            case default:
+            case command if command.startswith("echo "):
+                sys.stdout.write(f"{command[len("echo "):]}\n")
+            case "exit 0":
+                return 0
+            case command if command.startswith("type invalid"):
+                sys.stdout.write(f"{command[len('type '):]}: not found\n")
+            case command if command.startswith("type "):
+                sys.stdout.write(f"{command[len('type '):]} is a shell builtin\n")
+            case _:
                 sys.stdout.write(f"{command}: command not found\n")
-    return
 
 
 if __name__ == "__main__":
