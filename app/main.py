b927eca7747cca5a5c7aa15077c09f104987ee95
zacw-243L <qwerty.amousy123442x@gmail.com>
2025-06-24 20:58:29 +0800

    up arro2qaeq up fa

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import sys
 import os
 import shutil
@@ -16,8 +17,8 @@ def parser(string, as_list=False):
     is_escaped = False
 
     for x, char in enumerate(string):
-
         if is_escaped:
+            string_builder += char
             is_escaped = False
             continue
 
@@ -39,23 +40,27 @@ def parser(string, as_list=False):
                 is_double_quoted = True
             continue
 
-        if ord(char) == 92:
-            if is_single_quoted:
-                string_builder += char
-            elif is_double_quoted:
-                if string[x + 1] in (chr(92), '$', '"'):
+        if char == '\\':
+            if x + 1 < len(string):
+                if is_single_quoted:
+                    string_builder += char
+                elif is_double_quoted:
+                    if string[x + 1] in ('\\', '$', '"'):
+                        string_builder += string[x + 1]
+                        is_escaped = True
+                    else:
+                        string_builder += char
+                else:
                     string_builder += string[x + 1]
                     is_escaped = True
-                else:
-                    string_builder += char
-            else:
-                string_builder += string[x + 1]
-                is_escaped = True
             continue
 
         if not any([is_single_quoted, is_double_quoted]):
-
-            if char == ' ':
+            if char == '|' and not as_list:
+                result.append(string_builder.strip())
+                string_builder = str()
+                continue
+            if char == ' ' and as_list:
                 result.append(string_builder)
                 string_builder = str()
             else:
@@ -63,49 +68,41 @@ def parser(string, as_list=False):
         else:
             string_builder += char
 
-    result.append(string_builder)
+    if string_builder.strip():
+        result.append(string_builder.strip())
 
     while '' in result:
         result.remove('')
 
-    if as_list:
-        return result
-    else:
-        return ' '.join(result)
+    return result if as_list else ' '.join(result)
 
 
 def check_for_file_to_write(command):
     write_list = ['>', '1>', '2>', '>>', '1>>', '2>>']
+    append = False
+    err_flag = False
 
-    append = bool(command.count('>') - 1)
+    if isinstance(command, str):
+        command = parser(command, as_list=True)
 
     for x, symbol in enumerate(command):
-        if symbol == '>' and x:
-            if command[x - 1] == '2':
-                err_flag = True
-                break
-            else:
-                err_flag = False
+        if symbol in write_list:
+            append = symbol in ['>>', '1>>', '2>>']
+            err_flag = symbol in ['2>', '2>>']
+            output_file = command[x + 1].strip()
+            command = command[:x]
+            return (command, output_file, append, err_flag)
 
-    if any([x for x in command if x in write_list]):
-        io_splitter = command.replace('1>', '>').replace('2>', '>').replace('>>', '>').split('>')
-        write_command = io_splitter[0]
-        output_file = io_splitter[1]
-    else:
-        return (command, None, False, False)
-
-    return (write_command, output_file.strip(), append, err_flag)
+    return (command, None, False, False)
 
 
 def write_to(file, text, append=False):
-    filepath = file[::-1].split(chr(47), 1)[1][::-1]
-    file_name = file[::-1].split(chr(47), 1)[0][::-1]
+    filepath = file[::-1].split('/', 1)[1][::-1] if '/' in file else '.'
+    file_name = file[::-1].split('/', 1)[0][::-1]
 
-    os.chdir(filepath.strip())
-
-    open(file_name, 'a' if append else 'w').write(str(text))
-
-    return None
+    os.makedirs(filepath, exist_ok=True)
+    with open(os.path.join(filepath, file_name), 'a' if append else 'w') as f:
+        f.write(str(text))
 
 
 def check_fork(com):
@@ -122,7 +119,37 @@ class Autocomplete:
         if len(results) > 2:
             return results[symbol_iter]
         else:
-            return results[symbol_iter] + ' '
+            return results[symbol_iter] + ' ' if results[symbol_iter] else None
+
+
+def run_pipeline(commands, output_file=None, append=False, err_flag=False):
+    processes = []
+    prev_pipe_read = None
+
+    for i, cmd in enumerate(commands):
+        cmd_args = parser(cmd, as_list=True)
+        stdout = subprocess.PIPE if i < len(commands) - 1 else (open(output_file, 'a' if append else 'w') if output_file and not err_flag else sys.stdout)
+        stderr = open(output_file, 'a' if append else 'w') if output_file and err_flag else sys.stderr
+        stdin = prev_pipe_read if prev_pipe_read else None
+
+        try:
+            proc = subprocess.Popen(cmd_args, stdin=stdin, stdout=stdout, stderr=stderr, text=True)
+            processes.append(proc)
+        except FileNotFoundError:
+            print(f"{cmd_args[0]}: command not found", file=sys.stderr)
+            return
+
+        if prev_pipe_read:
+            prev_pipe_read.close()
+        prev_pipe_read = proc.stdout if i < len(commands) - 1 else None
+
+    for proc in processes:
+        proc.wait()
+
+    if output_file and not err_flag:
+        stdout.close()
+    if output_file and err_flag:
+        stderr.close()
 
 
 def main():
@@ -135,7 +162,6 @@ def main():
     flag_history_from_file = False
 
     command_list = ['exit', 'echo', 'type', 'pwd', 'cd', 'history']
-
     completer = Autocomplete(command_list)
     completer.commands = copy(command_list)
     dynamic_path = [folder for folder in
@@ -143,131 +169,134 @@ def main():
                     folder[:4] == '/tmp']
     dynamic_commands = []
     for folder in dynamic_path:
-        folder_list = subprocess.run(f'ls -1 {folder}', shell=True, capture_output=True).stdout.decode()
-        dynamic_commands.append(''.join(folder_list).strip())
-    dynamic_commands = sorted(dynamic_commands)
-    completer.commands.extend(dynamic_commands)
+        folder_list = subprocess.run(f'ls -1 {folder}', shell=True, capture_output=True).stdout.decode().splitlines()
+        dynamic_commands.extend(folder_list)
+    completer.commands.extend(sorted(dynamic_commands))
     readline.clear_history()
     readline.set_completer(completer.complete)
     readline.parse_and_bind('tab: complete')
+    readline.parse_and_bind('up: previous-history')
+    readline.parse_and_bind('down: next-history')
     readline.set_completer_delims('\t')
 
     while True:
+        try:
+            sys.stdout.write('$ ')
+            sys.stdout.flush()
+            command = input()
+            if not command.strip():
+                continue
 
-        output_file = None
-        append = None
-        err_flag = None
+            history_list.append(command)
 
-        command = input('$ ')
+            if check_fork(command):
+                commands = parser(command, as_list=True)
+                command, output_file, append, err_flag = check_for_file_to_write(commands[-1])
+                commands[-1] = ' '.join(command)
+                run_pipeline(commands, output_file, append, err_flag)
+                continue
 
-        command_foo = copy(command)
-        history_list.append(command_foo)
-
-        command, output_file, append, err_flag = check_for_file_to_write(command)
-
-        command_full = parser(command).split(' ', 1)
-        identifier = command_full[0]
-
-        if command[0] in ("'", '"'):
+            command, output_file, append, err_flag = check_for_file_to_write(command)
             command_full = parser(command, as_list=True)
-            command_full[0] = 'cat '
-            command = ''.join(command_full)
-            identifier = 'cat'
+            identifier = command_full[0]
 
-        if check_fork(command_foo):
-            os.fork()
+            if command[0] in ("'", '"'):
+                command_full = parser(command, as_list=True)
+                command_full[0] = 'cat '
+                command = ' '.join(command_full)
+                identifier = 'cat'
 
-        match identifier:
+            match identifier:
+                case 'exit':
+                    if os.getenv('HISTFILE'):
+                        with open(os.getenv('HISTFILE'), 'w') as h:
+                            for line in history_list:
+                                h.write(f'{line}\n')
+                    exit(int(command_full[1]) if len(command_full) > 1 else 0)
 
-            case 'exit':
-                if os.getenv('HISTFILE'):
-                    with open(os.getenv('HISTFILE'), 'w') as h:
-                        for x, line in enumerate(history_list):
-                            h.write(f'{line}\n')
-                        h.close()
-                exit(int(command_full[1]))
-
-            case 'echo':
-                if output_file:
-                    if err_flag:
-                        try:
-                            open(output_file, 'r')
-                        except FileNotFoundError:
-                            write_to(output_file, '', append=append)
-                            # print(command_full[1])
-                        finally:
-                            write_to(output_file, '', append=append)
-                            print(command_full[1])
+                case 'echo':
+                    output = command_full[1] if len(command_full) > 1 else ''
+                    if output_file:
+                        if err_flag:
+                            print(output, file=sys.stderr)
+                            write_to(output_file, output + '\n', append=append)
+                        else:
+                            write_to(output_file, output + '\n', append=append)
                     else:
-                        write_to(output_file, command_full[1] + '\n', append=append)
-                else:
-                    print(command_full[1])
+                        print(output)
 
-            case 'type':
-                if command_full[1].strip() in command_list:
-                    print(f'{command_full[1]} is a shell builtin')
-                elif PATH := shutil.which(command_full[1] if command_full[1] else ''):
-                    print(f'{command_full[1]} is {PATH}')
-                else:
-                    print(f'{command_full[1]}: not found')
+                case 'type':
+                    query = command_full[1].strip() if len(command_full) > 1 else ''
+                    if query in command_list:
+                        print(f'{query} is a shell builtin')
+                    elif PATH := shutil.which(query):
+                        print(f'{query} is {PATH}')
+                    else:
+                        print(f'{query}: not found')
 
-            case 'pwd':
-                print(os.getcwd())
+                case 'pwd':
+                    print(os.getcwd())
 
-            case 'cd':
-                if command_full[1] == '~':
-                    os.chdir(Path.home())
-                else:
+                case 'cd':
+                    path = command_full[1] if len(command_full) > 1 else Path.home()
+                    if path == '~':
+                        path = Path.home()
                     try:
-                        os.chdir(command_full[1])
+                        os.chdir(path)
                     except FileNotFoundError:
-                        print(f'cd: {command_full[1]}: No such file or directory')
+                        print(f'cd: {path}: No such file or directory')
 
-            case 'history':
-
-                if history_file is not None:
-                    with open(history_file, 'r') as hist_temp:
-                        curr_history_path = [l.rstrip() for l in hist_temp]
-                else:
-                    curr_history_path = history_list
-
-                if len(command_full) == 1:
-                    if flag_history_from_file == True:
-                        print(f' {1} history -r {history_file}')
-                    for x, line in enumerate(curr_history_path):
-                        print(f' {x + 1 + int(flag_history_from_file)} {line}')
-                    if flag_history_from_file == True:
-                        print(f' {x + 2} history')
-                else:
-                    if command_full[1][0] == '-':
-                        match command_full[1][1]:
-                            case 'r':
-                                history_file = command_full[1][3:]
-                                flag_history_from_file = True
-                            case 'w':
-                                history_file = command_full[1][3:]
-                                with open(history_file, 'w') as h:
-                                    for x, line in enumerate(history_list):
-                                        h.write(f'{line}\n')
-                            case 'a':
-                                history_file = command_full[1][3:]
-                                with open(history_file, 'a') as h:
-                                    for x, line in enumerate(history_list):
-                                        if x >= history_pointer:
-                                            h.write(f'{line}\n')
-                                history_pointer = x + 1
-
+                case 'history':
+                    if history_file is not None:
+                        with open(history_file, 'r') as hist_temp:
+                            curr_history_path = [l.rstrip() for l in hist_temp]
                     else:
-                        command_number = int(command_full[1])
-                        cut = len(history_list) - command_number
-                        for y, line in enumerate(history_list[cut:]):
-                            print(f' {y + 2 + command_number} {line}')
+                        curr_history_path = history_list
 
-            case default:
-                if identifier := shutil.which(identifier if identifier else ''):
-                    subprocess.run(command_foo, shell=True)
-                else:
-                    print(f'{command}: command not found')
+                    if len(command_full) == 1:
+                        if flag_history_from_file:
+                            print(f' {1} history -r {history_file}')
+                        for x, line in enumerate(curr_history_path):
+                            print(f' {x + 1 + int(flag_history_from_file)} {line}')
+                        if flag_history_from_file:
+                            print(f' {x + 2} history')
+                    else:
+                        if command_full[1].startswith('-'):
+                            option = command_full[1][1]
+                            history_file = command_full[1][3:] if len(command_full[1]) > 3 else ''
+                            if option == 'r':
+                                flag_history_from_file = True
+                                if os.path.exists(history_file):
+                                    with open(history_file, 'r') as h:
+                                        history_list.extend([l.rstrip() for l in h])
+                            elif option == 'w':
+                                with open(history_file, 'w') as h:
+                                    for line in history_list:
+                                        h.write(f'{line}\n')
+                            elif option == 'a':
+                                with open(history_file, 'a') as h:
+                                    for x, line in enumerate(history_list[history_pointer:]):
+                                        h.write(f'{line}\n')
+                                history_pointer = len(history_list)
+                        else:
+                            command_number = int(command_full[1])
+                            cut = max(0, len(history_list) - command_number)
+                            for y, line in enumerate(history_list[cut:]):
+                                print(f' {y + cut + 1} {line}')
+
+                case _:
+                    if identifier := shutil.which(identifier):
+                        proc = subprocess.run(command, shell=True, text=True,
+                                             stdout=open(output_file, 'a' if append else 'w') if output_file and not err_flag else sys.stdout,
+                                             stderr=open(output_file, 'a' if append else 'w') if output_file and err_flag else sys.stderr)
+                    else:
+                        print(f'{command}: command not found')
+
+        except KeyboardInterrupt:
+            print('^C')
+            continue
+        except EOFError:
+            break
 
 
 if __name__ == '__main__':
